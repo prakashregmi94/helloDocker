6c387a0386fa837f89319f407f0415580187ad74
Prakash <prakashregmi71@gmail.com>
2023-12-04 21:09:52 +1100

    Updated Readme file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const port = 8888
 app.get("/", (req, res) => {
-    res.send("Demo of Node application!!")
+    res.send("Demo of Node Application on Docker!")
 })
 
 app.listen(port, () => {
